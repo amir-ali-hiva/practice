858bbd20d751c349ce3dc3d43d2b4f5cacd9da15
amir-ali-hiva <amiralirahimian3@gmail.com>
2025-08-19 04:10:07 -0700

    change names to Stu_list_Tables

--- The_end/Bussinesslogic.py
+++ The_end/Bussinesslogic.py
@@ -4,7 +4,7 @@ class teacher:
     def __init__(self) -> None:
         self.dal = DataAccess()
     def insert_student(self, name, family, id, age, Score):
-        query = f"INSERT INTO Stu_list ('Name','Family','Age','Id','Score') VALUES ('{name}','{family}',{age},{id},{Score});"
+        query = f"INSERT INTO Stu_list_Tables ('Name','Family','Age','Id','Score') VALUES ('{name}','{family}',{age},{id},{Score});"
         message = self.dal.execute_non_query(query)
         return message
 class Student:
@@ -15,16 +15,16 @@ class maniger :
     def __init__(self) -> None:
         self.dal = DataAccess()    
     def insert_student(self, name, family, id, age, Score):
-        query = f"INSERT INTO Stu_list ('Name','Family','Age','Id','Score') VALUES ('{name}','{family}',{age},{id},{Score});"
+        query = f"INSERT INTO Stu_list_Tables ('Name','Family','Age','Id','Score') VALUES ('{name}','{family}',{age},{id},{Score});"
         message = self.dal.execute_non_query(query)
         return message
     
     def delete_stu(self, id):
-        query = f"DELETE FROM Stu_line WHERE Id={id}"
+        query = f"DELETE FROM Stu_list_Tables WHERE Id={id}"
         message = self.dal.execute_non_query(query)
         return message
     def show_stu(self):
-        query = "SELECT * FROM Stu_line;"
+        query = "SELECT * FROM Stu_list_Tables;"
         message = self.dal.execute_query(query)
         return message
 
